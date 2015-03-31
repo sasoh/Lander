cb93fa6707e0a73ed04d0ad6aa5c56b450ca22f3
Sasho <lxndrppv@gmail.com>
2015-03-31 21:01:03 +0300

    Adding project files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	[Range(0.0f, 100.0f)]
+	public float speed;
+
+	private float moveHorizontal = 0.0f;
+	private float moveVertical = 0.0f;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+		ProcessInput();
+
+	}
+
+	void FixedUpdate()
+	{
+
+		UpdateMovement();
+
+	}
+
+	void ProcessInput()
+	{
+
+		moveHorizontal = Input.GetAxis("Horizontal");
+		moveVertical = Input.GetAxis("Vertical");
+
+	}
+
+	void UpdateMovement()
+	{
+
+		Vector2 movementDirection = new Vector2(moveHorizontal, moveVertical);
+		Rigidbody2D rBody = this.GetComponent<Rigidbody2D>();
+		rBody.AddForce(movementDirection * speed);
+
+	}
+
+}
