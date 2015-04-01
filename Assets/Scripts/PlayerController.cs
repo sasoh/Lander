4eea2c2f06b2dbc236fb31a8b71b3ec0aa75bb97
Sasho <sasoh@Macintosh.local>
2015-04-01 13:32:47 +0300

    Adjusted values; limited player horizontal/vertical speed; moved material from player to platforms

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
 	[Range(0.0f, 100.0f)]
 	public float speed;
 
+	[Range(0.0f, 10.0f)]
+	public float maximumSpeedHorizontal = 4.0f;
+	
+	[Range(0.0f, 10.0f)]
+	public float maximumSpeedVertical = 4.0f;
+
 	private float moveHorizontal = 0.0f;
 	private float moveVertical = 0.0f;
 
@@ -43,6 +49,11 @@ public class PlayerController : MonoBehaviour
 		Vector2 movementDirection = new Vector2(moveHorizontal, moveVertical);
 		Rigidbody2D rBody = this.GetComponent<Rigidbody2D>();
 		rBody.AddForce(movementDirection * speed);
+
+		Vector2 velocity = rBody.velocity;
+		velocity.x = Mathf.Clamp(velocity.x, -100.0f, maximumSpeedHorizontal);
+		velocity.y = Mathf.Clamp(velocity.y, -100.0f, maximumSpeedVertical);
+		rBody.velocity = velocity;
 
 	}
 
