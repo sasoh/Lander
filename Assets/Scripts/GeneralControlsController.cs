083d4cf71c0c25806b29c613e3b664dc4ed7e260
Sasho <lxndrppv@gmail.com>
2015-04-04 03:10:10 +0300

    Added escape key for app quit; added temp & builds directory to .gitignore

---
+++ Assets/Scripts/GeneralControlsController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralControlsController : MonoBehaviour
+{
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		if (Input.GetKey("escape"))
+		{
+			Application.Quit();
+		}
+
+	}
+
+}
