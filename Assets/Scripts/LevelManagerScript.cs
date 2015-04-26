5f421551d10ba22ddd120c95648ea4835927554b
Sasho <lxndrppv@gmail.com>
2015-04-26 21:20:52 +0300

    Added console print when player delivered all cargo

---
+++ Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManagerScript : MonoBehaviour
+{
+
+	private bool victory = false;
+
+	// Use this for initialization
+	void Start()
+	{
+
+		victory = false;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+		if (HasCargoBoxesOnScene() == false && victory == false)
+		{
+			victory = true;
+
+			print("Player won!");
+		}
+
+	}
+
+	bool HasCargoBoxesOnScene()
+	{
+		
+		bool result = false;
+		
+		GameObject[] cargos = GameObject.FindGameObjectsWithTag("Cargo");
+		if (cargos.Length > 0)
+		{
+			result = true;
+		}
+
+		return result;
+
+	}
+}
