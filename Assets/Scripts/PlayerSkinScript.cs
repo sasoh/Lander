25fe829f8503b3ca6038b1740a50d3af14805f3d
Sasho <lxndrppv@gmail.com>
2015-04-27 23:30:56 +0300

    Added (rough) player vehicle frame customization

--- Assets/Scripts/PlayerSkinScript.cs
+++ Assets/Scripts/PlayerSkinScript.cs
@@ -5,6 +5,9 @@ public class PlayerSkinScript : MonoBehaviour
 {
 
 	public SpriteRenderer faceObject = null;
+	public GameObject body = null;
+	public GameObject body1prefab = null;
+	public GameObject body2prefab = null;
 
 	// Use this for initialization
 	void Start()
@@ -30,7 +33,47 @@ public class PlayerSkinScript : MonoBehaviour
 	{
 
 		Sprite faceSprite = LoadFaceSpriteFromSettings();
-		faceObject.sprite = faceSprite;
+		if (faceSprite != null)
+		{
+			faceObject.sprite = faceSprite;
+		}
+
+		// remove old frame
+		GameObject oldFrame = GameObject.FindGameObjectWithTag("Frame");
+		if (oldFrame != null)
+		{
+			Vector3 originalPosition = oldFrame.transform.localPosition;
+			Destroy(oldFrame);
+			GameObject newFrame = LoadNewFrameFromSettings();
+			if (newFrame != null)
+			{
+				newFrame.transform.parent = body.transform;
+				newFrame.transform.localPosition = originalPosition;
+			}
+		}
+	}
+
+	GameObject LoadNewFrameFromSettings()
+	{
+
+		GameObject result = null;
+
+		int index = 0;
+		if (PlayerPrefs.HasKey("BodyNumber") == true)
+		{
+			index = PlayerPrefs.GetInt("BodyNumber");
+		}
+
+		if (index == 0)
+		{
+			result = Instantiate(body1prefab);
+		}
+		else
+		{
+			result = Instantiate(body2prefab);
+		}
+
+		return result;
 
 	}
 
@@ -40,11 +83,11 @@ public class PlayerSkinScript : MonoBehaviour
 		Sprite result = null;
 
 		Sprite[] allFaces = Resources.LoadAll<Sprite>("Sprites/Player/Character/Faces");
-		
+
 		if (allFaces.Length > 0)
 		{
 			int index = 0; // default face index
-			
+
 			if (PlayerPrefs.HasKey("FaceNumber") == true)
 			{
 				index = PlayerPrefs.GetInt("FaceNumber");
